c987919bc17195da644a3c51d6d3dceba91b4112
rks9800 <rs994797@gmail.com>
2020-03-26 11:43:46 +0530

    temproary home page for users added

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 /**********Password Hashing Validator Module**********/
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-var validator = require('validator');
+var validator = require("validator");
 /***********Model Modules***********/
 const Admin = require("../model/Admin");
 /***********Nodemailer***********/
 const nodemailer = require("nodemailer");
 
-
 //Admin Login Page Starter
 //START
 //=======================================================================================================================================================
-router.get('/', function(req, res) {
-  // admins.create({Email: "ritesh@gmail.com", Password: "12345"}, function(err, obj) {
-  //   if(err)
-  //   {
+router.get("/", function(req, res) {
+  // Admin.create({ Email: "ritesh@gmail.com", Password: "12345" }, function(
+  //   err,
+  //   obj
+  // ) {
+  //   if (err) {
   //     console.log(err);
-  //   }
-  //   else{
+  //   } else {
   //     console.log("success");
   //   }
   // });
@@ -31,28 +31,26 @@ router.get('/', function(req, res) {
 //Admin Login Request Handel
 //START
 //=======================================================================================================================================================
-router.post("/Admin_login", function(req, res){
+router.post("/Admin_login", function(req, res) {
   var Admin_Login = JSON.parse(req.body.admin_login);
-  admins.findOne({ Email: Admin_Login.Email}, function(err, obj){
-    if(err){
+  Admin.findOne({ Email: Admin_Login.Email }, function(err, obj) {
+    if (err) {
       console.log(err);
-    }
-    else if(obj==null){
+    } else if (obj == null) {
       res.send([false, "no user found"]);
-    }
-    else{
+    } else {
       bcrypt.compare(Admin_Login.Password, obj.Password, function(err, result) {
-        if(err)
-        {
+        if (err) {
           console.log(err);
-        }
-        else if(result==false)
-        {
+        } else if (result == false) {
           console.log("password did not match");
-        }
-        else{
-          var token = jwt.sign({data: obj._id}, "blkhrt", {expiresIn: "1h"});
-          res.cookie("token", token, {maxAge: 3600000, httpOnly: true}).send([true]);
+        } else {
+          var token = jwt.sign({ data: obj._id }, "blkhrt", {
+            expiresIn: "1h"
+          });
+          res
+            .cookie("token", token, { maxAge: 3600000, httpOnly: true })
+            .send([true]);
           console.log(result);
         }
       });
@@ -84,6 +82,24 @@ function authToken(req, res, next) {
 //=======================================================================================================================================================
 router.get("/logout", function(req, res) {
   res.clearCookie("token").redirect("/");
+});
+//=======================================================================================================================================================
+//END
+
+//dashboard
+//START
+//=======================================================================================================================================================
+router.get("/dashboard", function(req, res) {
+  res.render("dashboard");
+});
+//=======================================================================================================================================================
+//END
+
+//home
+//START
+//=======================================================================================================================================================
+router.get("/home", function(req, res) {
+  res.render("home");
 });
 //=======================================================================================================================================================
 //END
